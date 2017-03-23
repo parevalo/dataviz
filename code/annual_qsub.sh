ffde31ea141268b502609f2fe39ed7999026d58e
Valerie <valpasq@bu.edu>
2017-03-22 20:09:55 -0400

    fixed last jobs - should run to end, not stop

--- code/annual_qsub.sh
+++ code/annual_qsub.sh
@@ -12,10 +12,9 @@ end=7170
 # run annual stats for image chunks
 for row1 in $(seq $start $step $stop); do
 	row2=`expr $row1 + $step`
-	qsub annual.sh -j y -V -l h_rt=48:00:00 -N stats_$row1 -b y \
-	$row1 $row2
+	qsub annual.sh $row1 $row2
 
 done
 
 # run last odd rows
-qsub annual.sh $row2 $stop
+qsub annual.sh $row2 $end
